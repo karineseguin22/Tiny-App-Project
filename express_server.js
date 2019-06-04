8bfa0d7b4c45157e54e61a04df45bc204fe8cad9
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-04 17:24:49 +0000

    created new directory, urls_index file, and added new get command to get urls from urls_index file

--- express_server.js
+++ express_server.js
@@ -4,6 +4,11 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
