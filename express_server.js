c010a2e9f9b284770ef3da70f2312ac1177694c1
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-06 00:59:09 +0000

    completed the username, make sure to require in future for cookie

--- express_server.js
+++ express_server.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
+var cookieParser = require('cookie-parser');
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 function generateRandomString() {
-return Math.floor((Math.random()*1000000000)).toString(36); 
+  return Math.floor((Math.random()*1000000000)).toString(36); 
 }
 
 var urlDatabase = {
@@ -16,75 +17,78 @@ var urlDatabase = {
     "9sm5xK": "http://www.google.com"
   };
 
-  app.get("/", (req, res) => {
-    res.send("Hello!");
-  });
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-//cookie 
+
+  //cookie 
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username)
-  console.log(req.body.username); 
+  debugger
+  res.cookie("username", req.body.username)
+  //console.log(req.body.username); 
 res.redirect('/urls'); 
 }); 
+
 //update post 
-  app.post('/urls/:shortURL/update', (req, res) => {
-    console.log('req body:', req.body); 
-    //extract id
-    const id = req.params.shortURL;
-    //extract updated website a
-    const info = req.body.longURL;
-    //update on object
-    urlDatabase[id]= info; 
-    console.log(urlDatabase);
-    //redirect 
-    res.redirect("/urls"); 
-  }); 
+app.post('/urls/:shortURL/update', (req, res) => {
+  console.log('req body:', req.body); 
+  //extract id
+  const id = req.params.shortURL;
+  //extract updated website a
+  const info = req.body.longURL;
+  //update on object
+  urlDatabase[id]= info; 
+  console.log(urlDatabase);
+  //redirect 
+  res.redirect("/urls"); 
+}); 
   
-  //post route
-  app.post("/urls", (req, res) => {
-    //create new id that will represent short url
-    const id = generateRandomString(); 
-    //obtain body from browser
-    //const newUrl = req.body; //this line was not necessary 
-    //add to object
-    urlDatabase[id] = req.body.longURL; 
-    //console.log(urlDatabase); //Log our new object 
-    //console.log(req.body);  // Log the POST request body to the console
-    //console.log(newUrl.longURL) //no longer valid 
-    //console.log(id); 
-    res.redirect(`/urls/${id}`);         // redirect to link of website we just added 
-  });
+//post route
+app.post("/urls", (req, res) => {
+  //create new id that will represent short url
+  const id = generateRandomString(); 
+  //obtain body from browser
+  //const newUrl = req.body; //this line was not necessary 
+  //add to object
+  urlDatabase[id] = req.body.longURL; 
+  //console.log(urlDatabase); //Log our new object 
+  //console.log(req.body);  // Log the POST request body to the console
+  //console.log(newUrl.longURL) //no longer valid 
+  //console.log(id); 
+  res.redirect(`/urls/${id}`);         // redirect to link of website we just added 
+});
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    //console.log(urlDatabase);
-    res.render("urls_index", templateVars);
-  });
+  let templateVars = { urls: urlDatabase, username: req.cookies === undefined ? "" : req.cookies.username };
+  res.render("urls_index", templateVars);
+});
 
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_news");
-  });
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_news");
+});
 
 
-  app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    //console.log(urlDatabase[req.params.shortURL]);
-    res.render("urls_show", templateVars);
-  });
 
-  //short version of "/urls/:shortURL" (not done)
-  //The goal is to go to long url 
-  app.get("/u/:shortURL", (req, res) => {
-    //const sURL = req.params.shortURL; //not neccesary 
-    const longURL = urlDatabase[req.params.shortURL];
-    //console.log(urlDatabase.b2xVn2); 
-    //console.log(req.params.shortURL);//gives shortURL ex: b2xVn2  
-    //console.log(sURL); // gives shortURL
-    //console.log(urlDatabase[sURL]);// need to put in square brakets when variable
+app.get("/urls/:shortURL", (req, res) => {
+  
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+  //console.log(urlDatabase[req.params.shortURL]);
+  res.render("urls_show", templateVars);
+});
 
-    res.redirect(longURL);
-  });
+//short version of "/urls/:shortURL" (not done)
+//The goal is to go to long url 
+app.get("/u/:shortURL", (req, res) => {
+  //const sURL = req.params.shortURL; //not neccesary 
+  const longURL = urlDatabase[req.params.shortURL];
+  //console.log(urlDatabase.b2xVn2); 
+  //console.log(req.params.shortURL);//gives shortURL ex: b2xVn2  
+  //console.log(sURL); // gives shortURL
+  //console.log(urlDatabase[sURL]);// need to put in square brakets when variable
+
+  res.redirect(longURL);
+});
 
     //delete
 app.post("/urls/:shortURL/delete", (req, res) =>{
